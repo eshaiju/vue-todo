4a01c5976a045befe429cbf23ca81755db90dbd9
Shaiju Edakulangara <eshaiju@gmail.com>
2019-04-02 23:38:44 +0530

    Integrated json placeholder api for mock backend

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,8 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 import Todos from './components/Todos'
 import Header from './components/layout/Header'
 import AddTodo from './components/AddTodo'
@@ -20,32 +22,26 @@ export default {
   },
   data() {
     return{
-      todos: [
-        {
-          id: 1,
-          title: 'Todo 1',
-          completed: false
-        },
-        {
-          id: 2,
-          title: 'Todo 2',
-          completed: true
-        },
-        {
-          id: 3,
-          title: 'Todo 3',
-          completed: false
-        }
-      ]
+      todos: []
     }
   },
   methods: {
     deleteTodo(id) {
-      this.todos = this.todos.filter(todo => todo.id !==id);
+      axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .then(res => this.todos = this.todos.filter(todo => todo.id !== id))
+      .catch(err => console.log(err))
     },
     addTodo(newTodo) {
-      this.todos = [...this.todos, newTodo];
+      const { title, completed } = newTodo;
+      axios.post('https://jsonplaceholder.typicode.com/todos', { title, completed})
+      .then(res => this.todos = [...this.todos, res.data])
+      .catch(err => console.log(err))
     }
+  },
+  created() {
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+    .then(res => this.todos = res.data)
+    .catch(err => console.log(err))
   }
 }
 </script>
