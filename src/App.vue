dc415de0c745afed7b4d8f85c697f71070144640
Shaiju Edakulangara <eshaiju@gmail.com>
2019-04-02 19:06:20 +0530

    Delete todo integrated

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
-    <Todos v-bind:todos="todos" />
+    <Header />
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
   </div>
 </template>
 
 <script>
-import Todos from './components/Todos.vue'
+import Todos from './components/Todos'
+import Header from './components/layout/Header'
 
 export default {
   name: 'app',
   components: {
-    Todos
+    Todos,
+    Header
   },
   data() {
     return{
@@ -32,6 +35,11 @@ export default {
         }
       ]
     }
+  },
+  methods: {
+    deleteTodo(id) {
+      this.todos = this.todos.filter(todo => todo.id !==id);
+    }
   }
 }
 </script>
@@ -45,5 +53,16 @@ export default {
   body {
     font-family: Arial, Helvetica, sans-serif;
     line-height: 1.4;
+  }
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+  .btn:hover {
+    background: #666;
   }
 </style>
